e20571627b866c984ebd1d719a77eb56408ae719
grandiosa iva <ggo24@yahoo.com>
2021-06-14 20:33:56 +0400

    Add project files.

---
+++ EFCore5WebApp.DAL/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCore5WebApp.Core.Entities;
+
+namespace EFCore5WebApp.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<LookUp> LookUps { get; set; }
+
+        private async Task SaveDemo()
+        {
+            var context = new AppDbContext();
+            await context.SaveChangesAsync();
+        }
+        public AppDbContext() : base()
+        { }
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            var context = new AppDbContext(
+            new DbContextOptionsBuilder<AppDbContext>().UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DemoDb; Trusted_Connection = True; MultipleActiveResultSets = true").Options);
+        }
+    }
+}
