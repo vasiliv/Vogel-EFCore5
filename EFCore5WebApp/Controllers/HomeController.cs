9bce4a15074080d962903b67869fc56e7416ddd3
grandiosa iva <ggo24@yahoo.com>
2021-06-15 17:37:44 +0400

    chapter 5 finished

--- EFCore5WebApp/Controllers/HomeController.cs
+++ EFCore5WebApp/Controllers/HomeController.cs
@@ -1,11 +1,11 @@
-﻿using EFCore5WebApp.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using EFCore5WebApp.Models;
 
 namespace EFCore5WebApp.Controllers
 {
--- EFCore5WebApp/Startup.cs
+++ EFCore5WebApp/Startup.cs
@@ -1,13 +1,15 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFCOre5WebApp.DAL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace EFCore5WebApp
 {
@@ -23,6 +25,9 @@ namespace EFCore5WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString(
+                "connection")));
             services.AddControllersWithViews();
         }
 
